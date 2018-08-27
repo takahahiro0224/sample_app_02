febf298ef8efc9edd69963df5da4013f503aaa7f
Takahiro Nakamura <taxman189@gmail.com>
2018-08-27 12:11:03 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -69,4 +69,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+  
+  test "should redirect following when not logged in"
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect foloowers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
