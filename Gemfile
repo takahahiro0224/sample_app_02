cd6ae6c4b4eaddbfd6d50b6f54188d10ea061210
Takahiro Nakamura <taxman189@gmail.com>
2018-09-14 05:35:16 +0000

    Add redis

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'counter_culture', '~> 1.8'
+gem 'redis',                   '3.3.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.url = 'wss://blooming-reef-77111.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'https://blooming-reef-77111.herokuapp.com/' ]
 end
